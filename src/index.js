095beab6be3d86c62a6c276577ec3a9430c43380
Svetlana <sveta-dekt@tut.by>
2021-08-20 13:58:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  let arr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+      let str = expr.substring(i, i + 10);
+      let newStr = str.replace(/00/g, '');
+      let newStr1 = newStr.replace(/10/g, '.');
+      let newStr2 = newStr1.replace(/11/g, '-');
+      let newStr3 = newStr2.replace(/[**,]+/g, ' ');
+      arr = newStr3.split('  ,');
+      result += arr[0] == ' ' ? ' ' : MORSE_TABLE[arr[0]];
+      console.log(result);
+  }
+  return result;
 }
 
-module.exports = {
-    decode
+// function decode(expr) {
+//   let result = '';
+//   let arr = [];
+//     for (let i = 0; i < expr.length; i += 10){
+//        let str = expr.substring(i, i + 10);
+//         let newStr = str.replace(/00/g, '');
+//         let newStr1 = newStr.replace(/10/g, '.');
+//         let newStr2 = newStr1.replace(/11/g, '-');
+//         let newStr3 = newStr2.replace(/[**,]+/g, '');  
+//         arr = newStr3.split('  ,');
+//         result += arr[0] == ' ' ? ' ' :  MORSE_TABLE[arr[0]];
+//         console.log(result);
+//     }
+//     return result;
+//   }
+    
+ //decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010");
+ 
+ module.exports = {
+       decode
 }
